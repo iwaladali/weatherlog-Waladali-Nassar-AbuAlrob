b079f12c67c170d3fb0d2c5df67085fb79c95d3c
Abed-Alwahab Nassar <aboodna98ar@gmail.com>
2026-06-02 09:50:26 -0700

    start analyzer class

--- weather_analyzer.py
+++ weather_analyzer.py
@@ -2,16 +2,4 @@ import numpy as np
 
 class WeatherAnalyzer:
 
-    def monthly_stats(self, logs: list) -> dict:
-        groups = {}
-
-        for r in logs:
-            key = r["date"][:7]  # YYYY-MM
-
-            if key not in groups:
-                groups[key] = []
-
-            groups[key].append(r)
-
-        stats = {}
-
+   pass
